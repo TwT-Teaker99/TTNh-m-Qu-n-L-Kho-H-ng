ea92785074fa627b3628b58e47f5586a9adfb1c3
TwT-Teaker99 <49748759+TwT-Teaker99@users.noreply.github.com>
2020-05-27 09:17:03 +0700

    Update QuayHang.cs

--- QuanLyKhoHang/QuanLyKhoHang/QuayHang.cs
+++ QuanLyKhoHang/QuanLyKhoHang/QuayHang.cs
@@ -24,7 +24,7 @@ namespace QuanLyKhoHang
         public QuayHang()
         {
             InitializeComponent();
-            funcShare = new functionShare(0);
+            funcShare = new functionShare(2);
         }
         private bool isNumber(string pValue)
         {
